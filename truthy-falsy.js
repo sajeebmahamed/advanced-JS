7d8ffbe0b81edd36a5c87ed0aee7475ebe659727
Sajeeb <sajeebahamed6@gmail.com>
2020-03-04 00:51:10 +0600

    true vs false added

---
+++ truthy-falsy.js
@@ -0,0 +1,50 @@
+let name = 0;
+if(name || name == 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+
+// const age = '';
+// if(age){
+//     console.log("Condition is true");
+// }
+// else{
+//     console.log("Condition is false");
+// }
+
+                        /*******                            
+                         * Example of Falsy *******/
+                        /*
+                                if (false)
+                                if (null)
+                                if (undefined)
+                                if (0)
+                                if (0n)
+                                if (NaN)
+                                if ('')
+                                if ("")
+                                if (``)
+                                if (document.all)
+                        */
+
+                            /*******
+                             * Example of Truthy *******/
+                            /*
+                                if (true)
+                                if ({})
+                                if ([])
+                                if (42)
+                                if ("0")
+                                if ("false")
+                                if (new Date())
+                                if (-42)
+                                if (12n)
+                                if (3.14)
+                                if (-3.14)
+                                if (Infinity)
+                                if (-Infinity)
+                                */
