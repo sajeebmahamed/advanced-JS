2cae93b4093e7e2df128554775781229ae9ac48b
Sajeeb <sajeebahamed6@gmail.com>
2020-03-04 11:02:56 +0600

    ==C:/Program Files/Git/===

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,21 @@
+const a = 5;
+const b = "5";
+if(a == b){
+    //console.log("Condition is true");
+}
+else{
+    //console.log("Condition is false");
+}
+
+if (a === b) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
+/************
+"=="   => only value checked => type ta k convert kore dekhbe kachakachi ki value ase, tarpor sheta
+            k eki type e convert kore, compare kore dekhbe thik ase kina
+"==="  => value and type both will be checked
+
+ *******/
